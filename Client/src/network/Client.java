a9585d4770e3fe5a1ed877c5f516a16da900b3f0
fabianramos01 <fabian.ramos01@uptc.edu.co>
2018-05-18 10:12:46 -0500

    Agregue el archivo de donde obtengo las palabras, y ya las envia al cliente despues de hacer la petición al servidor

--- Client/src/network/Client.java
+++ Client/src/network/Client.java
@@ -1,8 +1,11 @@
 package network;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+
+import javax.swing.JOptionPane;
 
 public class Client {
 
@@ -14,7 +17,13 @@ public class Client {
 			socket = new Socket("localhost", 2000);
 			DataOutputStream output = new DataOutputStream(socket.getOutputStream());
 			System.out.println("enviando datos...");
-			output.writeUTF("hola");
+			int n = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de palabras"));
+			output.writeInt(n);
+			DataInputStream input = new DataInputStream(socket.getInputStream());
+			for (int i = 0; i < n; i++) {
+				System.out.println(input.readUTF());
+			}
+			input.close();
 			output.close();
 		} catch (IOException e) {
 			e.printStackTrace();
--- Server/src/network/Server.java
+++ Server/src/network/Server.java
@@ -1,6 +1,7 @@
 package network;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -20,9 +21,19 @@ public class Server {
 			Socket socket = serverSocket.accept();
 			System.out.println("New connection!");
 			DataInputStream input = new DataInputStream(socket.getInputStream());
-			System.out.println(input.readUTF());
+			int n = input.readInt();
+			System.out.println(n);
+			sendWords(n, new DataOutputStream(socket.getOutputStream()));
 			input.close();
 		}
+	}
+	
+	private void sendWords(int n, DataOutputStream output) throws IOException {
+		String[] words = {"hello", "good", "pool", "do", "come"};
+		for (int i = 0; i < n; i++) {
+			output.writeUTF(words[i]);
+		}
+		output.close();
 	}
 	
 	public static void main(String[] args) {
